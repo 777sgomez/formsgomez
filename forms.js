1fa2f413c9f93455948c0d8c706d9ff7fa95f2dd
Susan Gomez <susangomez@Susans-MacBook-Pro.local>
2023-07-13 18:15:12 -0400

    Added a document.querySelector to form and added an EventListener submit and function. Then added a preventDefault to that event to forms.js

--- forms.js
+++ forms.js
@@ -1,3 +1,7 @@
 let todo = [];
 const ul = document.createElement("ul");
 const body = document.querySelector("body");
+
+document.querySelector('form').addEventListener("submit", function(event) {
+    event.preventDefault();
+})
