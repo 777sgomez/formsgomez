a78e58ff8f1831e3c90158ee78d4c2f7521fd730
Susan Gomez <susangomez@Susans-MacBook-Pro.local>
2023-07-13 18:17:33 -0400

    Added a template and map to add the list item to the body of the html to forms.js

--- forms.js
+++ forms.js
@@ -5,4 +5,8 @@ const body = document.querySelector("body");
 document.querySelector('form').addEventListener("submit", function(event) {
     event.preventDefault();
     todo.push(document.getElementById("todoInput").value);
+    const template = todo.map (item => 
+        `
+        <li>${item}</li>
+        `)
 })
